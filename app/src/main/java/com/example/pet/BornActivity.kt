2999c7d614f92daa86ec111c7f12f1ade7b73160
Konstantin Safonov <ko.94@mail.ru>
2024-01-14 21:25:08 +0300

    какая-то ошибка, просто вылетает при переходе на другой экран

---
+++ app/src/main/java/com/example/pet/BornActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pet
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BornActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_born)
+    }
+}
--- app/src/main/java/com/example/pet/MainActivity.kt
+++ app/src/main/java/com/example/pet/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.pet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonNexScreen = findViewById<Button>(R.id.buttonContinue)
+        buttonNexScreen.setOnClickListener {
+            val intent = Intent(this, BornActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
