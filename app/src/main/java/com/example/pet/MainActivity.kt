8bb1c9054d953a227ffb06c9ec43d1f08ad1a189
Konstantin Safonov <ko.94@mail.ru>
2024-01-15 06:32:11 +0300

    исправил кнопку, просто забыл добавить вторую активити в манифест и вынес нажатие кнопки в отдельную функцию

--- app/src/main/java/com/example/pet/MainActivity.kt
+++ app/src/main/java/com/example/pet/MainActivity.kt
@@ -3,17 +3,25 @@ package com.example.pet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val buttonNexScreen = findViewById<Button>(R.id.buttonContinue)
+        nextScreenButton(buttonNexScreen)
+
+    }
+
+    fun nextScreenButton(buttonNexScreen: View) {
         buttonNexScreen.setOnClickListener {
             val intent = Intent(this, BornActivity::class.java)
             startActivity(intent)
         }
     }
+
+
 }
